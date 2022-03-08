1c3b3519a989387f2768b1000375fc387731e444
Feiye Chen <fchen82@myseneca.ca>
2022-03-08 00:48:43 -0500

    mobile Link

--- src/components/NavbarMobile.js
+++ src/components/NavbarMobile.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 const NavbarMobile = () => {
   return (
     <div className='mobile-menu'>
-        <div><a href=''>Search</a></div>
-        <div><a href='/resorts'>All Resorts</a></div>
-        <div><a href='/login'>Login</a></div>
-        <div><a href='/signup'>Register</a></div>
+        <div><Link to='/'>Search</Link></div>
+        <div><Link to='/resorts'>All Resorts</Link></div>
+        <div><Link to='/login'>Login</Link></div>
+        <div><Link to='/signup'>Register</Link></div>
     </div>
   )
 }

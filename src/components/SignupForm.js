b91d12c799d8cff63c095ada8b5ec19e9969dcbc
Feiye Chen <fchen82@myseneca.ca>
2022-03-12 14:13:44 -0500

    add POST to json-server

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignupForm = () => {
+
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(email !== '' && username !== '' && password !== '') {
+      const user = { email, username, password };
+
+      fetch("https://yotour-server.herokuapp.com/users", {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user)
+      }).then(() => {
+        setEmail("");
+        setUsername("");
+        setPassword("");
+      }).then(() => {
+        window.alert("Successfully registered!");
+      })
+    } else {
+      window.alert("All fields are required!");
+    }
+
+  }
+
   return (
     <div className='signup'>
 
@@ -8,10 +36,29 @@ const SignupForm = () => {
 
         <div className='signup-wrapper'>
 
-            <input className='login-input' type='text' placeholder='email' />
-            <input className='login-input' type='text' placeholder='Username' />
-            <input className='login-input' type='text' placeholder='Password' />
-            <button className='submit'>Sign Up</button>
+            <input 
+              className='login-input' 
+              type='text'
+              required
+              placeholder='Email' 
+              value={email}
+              onChange = {e => setEmail(e.target.value)} />
+            <input 
+              className='login-input' 
+              type='text'
+              required
+              placeholder='Username'
+              value={username}
+              onChange = {e => setUsername(e.target.value)} />
+            <input 
+              className='login-input' 
+              type='text'
+              required
+              placeholder='Password' 
+              value={password}
+              onChange = {e => setPassword(e.target.value)} />
+
+            <button className='submit' onClick={handleSubmit}>Sign Up</button>
                        
         </div>
 
